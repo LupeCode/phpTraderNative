102b7ab1cabd579ea33c80ab662b5ab207cb1dd5
Joshua <joshua@lupecode.com>
2018-01-18 14:27:52 -0800

    Converted the members and the SetCandleSettings function.

--- source/Core.php
+++ source/Core.php
@@ -4,102 +4,63 @@ namespace LupeCode\phpTraderNative;
 
 class Core
 {
-//    private int[] unstablePeriod;
-//
-//    private CandleSetting[] candleSettings;
-//
-//    private Compatibility compatibility;
-//
-//    /**
-//     * Creates a new instance of Core
-//     */
-//    public Core() {
-//        unstablePeriod = new int[com.tictactec.ta.lib.FuncUnstId.All
-//                .ordinal()];
-//        compatibility = Compatibility.Default;
-//        candleSettings = new CandleSetting[com.tictactec.ta.lib.CandleSettingType.AllCandleSettings
-//                .ordinal()];
-//        for (int i = 0; i < candleSettings.length; i++) {
-//            candleSettings[i] = new CandleSetting(TA_CandleDefaultSettings[i]);
-//        }
-//    }
-//
-//    public RetCode SetCandleSettings(CandleSettingType settingType,
-//                                     RangeType rangeType, int avgPeriod, double factor) {
-//        if (settingType.ordinal() >= CandleSettingType.AllCandleSettings
-//                .ordinal())
-//            return RetCode.BadParam;
-//        candleSettings[settingType.ordinal()].settingType = settingType;
-//        candleSettings[settingType.ordinal()].rangeType = rangeType;
-//        candleSettings[settingType.ordinal()].avgPeriod = avgPeriod;
-//        candleSettings[settingType.ordinal()].factor = factor;
-//        return RetCode.Success;
-//    }
-//
-//    final private CandleSetting TA_CandleDefaultSettings[] = {
-//      /*
-//      * real body is long when it's longer than the average of the 10
-//      * previous candles' real body
-//      */
-//            new CandleSetting(CandleSettingType.BodyLong,
-//                    RangeType.RealBody, 10, 1.0),
-//      /*
-//      * real body is very long when it's longer than 3 times the average
-//      * of the 10 previous candles' real body
-//      */
-//            new CandleSetting(CandleSettingType.BodyVeryLong,
-//                    RangeType.RealBody, 10, 3.0),
-//      /*
-//      * real body is short when it's shorter than the average of the 10
-//      * previous candles' real bodies
-//      */
-//            new CandleSetting(CandleSettingType.BodyShort,
-//                    RangeType.RealBody, 10, 1.0),
-//      /*
-//      * real body is like doji's body when it's shorter than 10% the
-//      * average of the 10 previous candles' high-low range
-//      */
-//            new CandleSetting(CandleSettingType.BodyDoji,
-//                    RangeType.HighLow, 10, 0.1),
-//      /* shadow is long when it's longer than the real body */
-//            new CandleSetting(CandleSettingType.ShadowLong,
-//                    RangeType.RealBody, 0, 1.0),
-//      /* shadow is very long when it's longer than 2 times the real body */
-//            new CandleSetting(CandleSettingType.ShadowVeryLong,
-//                    RangeType.RealBody, 0, 2.0),
-//      /*
-//      * shadow is short when it's shorter than half the average of the 10
-//      * previous candles' sum of shadows
-//      */
-//            new CandleSetting(CandleSettingType.ShadowShort,
-//                    RangeType.Shadows, 10, 1.0),
-//      /*
-//      * shadow is very short when it's shorter than 10% the average of
-//      * the 10 previous candles' high-low range
-//      */
-//            new CandleSetting(CandleSettingType.ShadowVeryShort,
-//                    RangeType.HighLow, 10, 0.1),
-//      /* when measuring distance between parts of candles or width of gaps */
-//      /*
-//      * "near" means "<= 20% of the average of the 5 previous candles'
-//      * high-low range"
-//      */
-//            new CandleSetting(CandleSettingType.Near,
-//                    RangeType.HighLow, 5, 0.2),
-//      /* when measuring distance between parts of candles or width of gaps */
-//      /*
-//      * "far" means ">= 60% of the average of the 5 previous candles'
-//      * high-low range"
-//      */
-//            new CandleSetting(CandleSettingType.Far,
-//                    RangeType.HighLow, 5, 0.6),
-//      /* when measuring distance between parts of candles or width of gaps */
-//      /*
-//      * "equal" means "<= 5% of the average of the 5 previous candles'
-//      * high-low range"
-//      */
-//            new CandleSetting(CandleSettingType.Equal,
-//                    RangeType.HighLow, 5, 0.05)};
+    /** @var int[] */
+    protected $unstablePeriod;
+    /** @var CandleSetting[] */
+    protected $candleSettings;
+    /** @var int */
+    protected $compatibility;
+
+    public function __construct()
+    {
+        $this->TA_CandleDefaultSettings = $this->TA_CandleDefaultSettings();
+        $this->unstablePeriod = [FuncUnstId::ALL];
+        $this->compatibility = Compatibility::Default;
+        $this->candleSettings = \array_fill(0, CandleSettingType::AllCandleSettings, null);
+        for($i = 0; $i < count($this->candleSettings); $i++) {
+            $this->candleSettings[$i] = new CandleSetting($this->TA_CandleDefaultSettings[$i]);
+        }
+    }
+
+    public function SetCandleSettings(int $settingType, int $rangeType, int $avgPeriod, float $factor)
+    {
+        if($settingType >= CandleSettingType::AllCandleSettings)
+            return RetCode::BadParam;
+        $this->candleSettings[$settingType]->settingType = $settingType;
+        $this->candleSettings[$settingType]->rangeType = $rangeType;
+        $this->candleSettings[$settingType]->avgPeriod = $avgPeriod;
+        $this->candleSettings[$settingType]->factor = $factor;
+        return RetCode::Success;
+    }
+
+    protected $TA_CandleDefaultSettings;
+    final protected function TA_CandleDefaultSettings()
+    {
+        return [
+            /* real body is long when it's longer than the average of the 10 previous candles' real body */
+            new CandleSetting(CandleSettingType::BodyLong, RangeType::RealBody, 10, 1.),
+            /* real body is very long when it's longer than 3 times the average of the 10 previous candles' real body */
+            new CandleSetting(CandleSettingType::BodyVeryLong, RangeType::RealBody, 10, 3.),
+            /* real body is short when it's shorter than the average of the 10 previous candles' real bodies */
+            new CandleSetting(CandleSettingType::BodyShort, RangeType::RealBody, 10, 1.),
+            /* real body is like doji's body when it's shorter than 10% the average of the 10 previous candles' high-low range */
+            new CandleSetting(CandleSettingType::BodyDoji, RangeType::HighLow, 10, 0.1),
+            /* shadow is long when it's longer than the real body */
+            new CandleSetting(CandleSettingType::ShadowLong, RangeType::RealBody, 0, 1.),
+            /* shadow is very long when it's longer than 2 times the real body */
+            new CandleSetting(CandleSettingType::ShadowVeryLong, RangeType::RealBody, 0, 2.),
+            /* shadow is short when it's shorter than half the average of the 10 previous candles' sum of shadows */
+            new CandleSetting(CandleSettingType::ShadowShort, RangeType::Shadows, 10, 1.),
+            /* shadow is very short when it's shorter than 10% the average of the 10 previous candles' high-low range */
+            new CandleSetting(CandleSettingType::ShadowVeryShort, RangeType::HighLow, 10, 0.1),
+            /* when measuring distance between parts of candles or width of gaps "near" means "<= 20% of the average of the 5 previous candles' high-low range" */
+            new CandleSetting(CandleSettingType::Near, RangeType::HighLow, 5, 0.2),
+            /* when measuring distance between parts of candles or width of gaps "far" means ">= 60% of the average of the 5 previous candles' high-low range" */
+            new CandleSetting(CandleSettingType::Far, RangeType::HighLow, 5, 0.6),
+            /* when measuring distance between parts of candles or width of gaps "equal" means "<= 5% of the average of the 5 previous candles' high-low range" */
+            new CandleSetting(CandleSettingType::Equal, RangeType::HighLow, 5, 0.05),
+        ];
+    }
 //
 //    public RetCode RestoreCandleDefaultSettings(
 //            CandleSettingType settingType) {
