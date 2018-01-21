7a5aa970e54689562040a8fb28546ecb9e198ea0
Joshua <joshua@lupecode.com>
2018-01-20 20:04:52 -0800

    Formatting, basically.

--- source/Core.php
+++ source/Core.php
@@ -1096,60 +1096,66 @@ class Core
         return RetCode::Success;
     }
 
-    public function bbandsLookback(int $optInTimePeriod,                              double $optInNbDevUp,                              double $optInNbDevDn,                              MAType $optInMAType): int {
-        if ((int) $optInTimePeriod == (PHP_INT_MIN))
+    public function bbandsLookback(int $optInTimePeriod, double $optInNbDevUp, double $optInNbDevDn, MAType $optInMAType): int
+    {
+        if ((int)$optInTimePeriod == (PHP_INT_MIN)) {
             $optInTimePeriod = 5;
-        elseif (((int) $optInTimePeriod < 2) || ((int) $optInTimePeriod > 100000))
+        } elseif (((int)$optInTimePeriod < 2) || ((int)$optInTimePeriod > 100000)) {
             return -1;
-        if ($optInNbDevUp == (-4e+37))
+        }
+        if ($optInNbDevUp == (-4e+37)) {
             $optInNbDevUp = 2.000000e+0;
-        elseif (($optInNbDevUp < -3.000000e+37) || ($optInNbDevUp > 3.000000e+37))
+        } elseif (($optInNbDevUp < -3.000000e+37) || ($optInNbDevUp > 3.000000e+37)) {
             return -1;
-        if ($optInNbDevDn == (-4e+37))
+        }
+        if ($optInNbDevDn == (-4e+37)) {
             $optInNbDevDn = 2.000000e+0;
-        elseif (($optInNbDevDn < -3.000000e+37) || ($optInNbDevDn > 3.000000e+37))
+        } elseif (($optInNbDevDn < -3.000000e+37) || ($optInNbDevDn > 3.000000e+37)) {
             return -1;
+        }
+
         return $this->movingAverageLookback($optInTimePeriod, $optInMAType);
     }
 
     /**
      * @param int                                $startIdx
      * @param int                                $endIdx
-     * @param double[]                              $inReal
+     * @param double[]                           $inReal
      * @param int                                $optInTimePeriod
      * @param float                              $optInNbDevUp
      * @param float                              $optInNbDevDn
      * @param \LupeCode\phpTraderNative\MAType   $optInMAType
      * @param \LupeCode\phpTraderNative\MInteger $outBegIdx
      * @param \LupeCode\phpTraderNative\MInteger $outNBElement
-     * @param double[]                              $outRealUpperBand
-     * @param double[]                              $outRealMiddleBand
-     * @param double[]                              $outRealLowerBand
+     * @param double[]                           $outRealUpperBand
+     * @param double[]                           $outRealMiddleBand
+     * @param double[]                           $outRealLowerBand
      *
      * @return int
      */
-    public function bbands(int $startIdx,                          int $endIdx,                          array $inReal,                          int $optInTimePeriod,                          double $optInNbDevUp,                          double $optInNbDevDn,                          MAType $optInMAType,                         MInteger &$outBegIdx,                         MInteger &$outNBElement,                          array &$outRealUpperBand,                          array &$outRealMiddleBand,                          array &$outRealLowerBand): int {
-        //RetCode $retCode;
-        //int $i;
-        //double $tempReal, $tempReal2;
-        //double[] $tempBuffer1;
-        //double[] $tempBuffer2;
-        if ($startIdx < 0)
+    public function bbands(int $startIdx, int $endIdx, array $inReal, int $optInTimePeriod, double $optInNbDevUp, double $optInNbDevDn, MAType $optInMAType, MInteger &$outBegIdx, MInteger &$outNBElement, array &$outRealUpperBand, array &$outRealMiddleBand, array &$outRealLowerBand): int
+    {
+        if ($startIdx < 0) {
             return RetCode::OutOfRangeStartIndex;
-        if (($endIdx < 0) || ($endIdx < $startIdx))
+        }
+        if (($endIdx < 0) || ($endIdx < $startIdx)) {
             return RetCode::OutOfRangeEndIndex;
-        if ((int) $optInTimePeriod == (PHP_INT_MIN))
+        }
+        if ((int)$optInTimePeriod == (PHP_INT_MIN)) {
             $optInTimePeriod = 5;
-        elseif (((int) $optInTimePeriod < 2) || ((int) $optInTimePeriod > 100000))
+        } elseif (((int)$optInTimePeriod < 2) || ((int)$optInTimePeriod > 100000)) {
             return RetCode::BadParam;
-        if ($optInNbDevUp == (-4e+37))
+        }
+        if ($optInNbDevUp == (-4e+37)) {
             $optInNbDevUp = 2.000000e+0;
-        elseif (($optInNbDevUp < -3.000000e+37) || ($optInNbDevUp > 3.000000e+37))
+        } elseif (($optInNbDevUp < -3.000000e+37) || ($optInNbDevUp > 3.000000e+37)) {
             return RetCode::BadParam;
-        if ($optInNbDevDn == (-4e+37))
+        }
+        if ($optInNbDevDn == (-4e+37)) {
             $optInNbDevDn = 2.000000e+0;
-        elseif (($optInNbDevDn < -3.000000e+37) || ($optInNbDevDn > 3.000000e+37))
+        } elseif (($optInNbDevDn < -3.000000e+37) || ($optInNbDevDn > 3.000000e+37)) {
             return RetCode::BadParam;
+        }
         if ($inReal == $outRealUpperBand) {
             $tempBuffer1 = $outRealMiddleBand;
             $tempBuffer2 = $outRealLowerBand;
@@ -1163,69 +1169,67 @@ class Core
             $tempBuffer1 = $outRealMiddleBand;
             $tempBuffer2 = $outRealUpperBand;
         }
-        if (($tempBuffer1 == $inReal) || ($tempBuffer2 == $inReal))
+        if (($tempBuffer1 == $inReal) || ($tempBuffer2 == $inReal)) {
             return RetCode::BadParam;
-        $retCode = $this->movingAverage($startIdx, $endIdx, $inReal,
-                $optInTimePeriod, $optInMAType,
-                $outBegIdx, $outNBElement, $tempBuffer1);
-        if (($retCode != RetCode::Success) || ((int) $outNBElement->value == 0)) {
+        }
+        $retCode = $this->movingAverage($startIdx, $endIdx, $inReal, $optInTimePeriod, $optInMAType, $outBegIdx, $outNBElement, $tempBuffer1);
+        if (($retCode != RetCode::Success) || ((int)$outNBElement->value == 0)) {
             $outNBElement->value = 0;
+
             return $retCode;
         }
         if ($optInMAType == MAType::SMA) {
-            $this->TA_INT_stddev_using_precalc_ma($inReal, $tempBuffer1,
-                    (int) $outBegIdx->value, (int) $outNBElement->value,
-                    $optInTimePeriod, $tempBuffer2);
+            $this->TA_INT_stddev_using_precalc_ma($inReal, $tempBuffer1, (int)$outBegIdx->value, (int)$outNBElement->value, $optInTimePeriod, $tempBuffer2);
         } else {
-            $retCode = $this->stdDev((int) $outBegIdx->value, $endIdx, $inReal,
-                    $optInTimePeriod, 1.0,
-                    $outBegIdx, $outNBElement, $tempBuffer2);
+            $retCode = $this->stdDev((int)$outBegIdx->value, $endIdx, $inReal, $optInTimePeriod, 1.0, $outBegIdx, $outNBElement, $tempBuffer2);
             if ($retCode != RetCode::Success) {
                 $outNBElement->value = 0;
+
                 return $retCode;
             }
         }
         if ($tempBuffer1 != $outRealMiddleBand) {
-            System.arraycopy($tempBuffer1, 0, $outRealMiddleBand, 0, $outNBElement->value);
+            $outRealMiddleBand = \array_slice($tempBuffer1, 0, $outNBElement->value);
         }
         if ($optInNbDevUp == $optInNbDevDn) {
             if ($optInNbDevUp == 1.0) {
-                for ($i = 0; $i < (int) $outNBElement->value; $i++) {
-                    $tempReal = $tempBuffer2[$i];
-                    $tempReal2 = $outRealMiddleBand[$i];
+                for ($i = 0; $i < (int)$outNBElement->value; $i++) {
+                    $tempReal             = $tempBuffer2[$i];
+                    $tempReal2            = $outRealMiddleBand[$i];
                     $outRealUpperBand[$i] = $tempReal2 + $tempReal;
                     $outRealLowerBand[$i] = $tempReal2 - $tempReal;
                 }
             } else {
-                for ($i = 0; $i < (int) $outNBElement->value; $i++) {
-                    $tempReal = $tempBuffer2[$i] * $optInNbDevUp;
-                    $tempReal2 = $outRealMiddleBand[$i];
+                for ($i = 0; $i < (int)$outNBElement->value; $i++) {
+                    $tempReal             = $tempBuffer2[$i] * $optInNbDevUp;
+                    $tempReal2            = $outRealMiddleBand[$i];
                     $outRealUpperBand[$i] = $tempReal2 + $tempReal;
                     $outRealLowerBand[$i] = $tempReal2 - $tempReal;
                 }
             }
         } elseif ($optInNbDevUp == 1.0) {
-            for ($i = 0; $i < (int) $outNBElement->value; $i++) {
-                $tempReal = $tempBuffer2[$i];
-                $tempReal2 = $outRealMiddleBand[$i];
+            for ($i = 0; $i < (int)$outNBElement->value; $i++) {
+                $tempReal             = $tempBuffer2[$i];
+                $tempReal2            = $outRealMiddleBand[$i];
                 $outRealUpperBand[$i] = $tempReal2 + $tempReal;
                 $outRealLowerBand[$i] = $tempReal2 - ($tempReal * $optInNbDevDn);
             }
         } elseif ($optInNbDevDn == 1.0) {
-            for ($i = 0; $i < (int) $outNBElement->value; $i++) {
-                $tempReal = $tempBuffer2[$i];
-                $tempReal2 = $outRealMiddleBand[$i];
+            for ($i = 0; $i < (int)$outNBElement->value; $i++) {
+                $tempReal             = $tempBuffer2[$i];
+                $tempReal2            = $outRealMiddleBand[$i];
                 $outRealLowerBand[$i] = $tempReal2 - $tempReal;
                 $outRealUpperBand[$i] = $tempReal2 + ($tempReal * $optInNbDevUp);
             }
         } else {
-            for ($i = 0; $i < (int) $outNBElement->value; $i++) {
-                $tempReal = $tempBuffer2[$i];
-                $tempReal2 = $outRealMiddleBand[$i];
+            for ($i = 0; $i < (int)$outNBElement->value; $i++) {
+                $tempReal             = $tempBuffer2[$i];
+                $tempReal2            = $outRealMiddleBand[$i];
                 $outRealUpperBand[$i] = $tempReal2 + ($tempReal * $optInNbDevUp);
                 $outRealLowerBand[$i] = $tempReal2 - ($tempReal * $optInNbDevDn);
             }
         }
+
         return RetCode::Success;
     }
 
