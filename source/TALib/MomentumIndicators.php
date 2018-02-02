df7b3bfb6efa0b586e3ec1c7dac73a197edce688
Joshua <joshua@lupecode.com>
2018-02-01 23:13:29 -0800

    Added the extraction classes to make the TA Lib Core smaller.

---
+++ source/TALib/MomentumIndicators.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace TALib;
+
+class MomentumIndicators
+{
+
+    //<editor-fold defaultstate="collapsed" desc="Protected Members">
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Constant Members">
+
+    //</editor-fold>
+
+    //<editor-fold defaultstate="collapsed" desc="Getters and Setters">
+
+    //</editor-fold>
+
+    public function __construct()
+    {
+
+    }
+
+}
