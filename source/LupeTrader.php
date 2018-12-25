e7a0b95dd82c658114ff67252d0f072d8edc6f25
Joshua <joshua@lupecode.com>
2018-12-24 20:09:14 -0800

    Added the Slow Stoch RSI function.

--- source/LupeTrader.php
+++ source/LupeTrader.php
@@ -2,7 +2,41 @@
 
 namespace LupeCode\phpTraderNative;
 
+use LupeCode\phpTraderNative\LupeTrader\Core\MomentumIndicators;
+use LupeCode\phpTraderNative\TALib\Enum\MovingAverageType;
+use LupeCode\phpTraderNative\TALib\Enum\ReturnCode;
+
 class LupeTrader extends Trader
 {
+
+    /**
+     * @param array $real
+     * @param int   $rsi_period
+     * @param int   $fastK_Period
+     * @param int   $slowK_Period
+     * @param int   $slowK_MAType
+     * @param int   $slowD_Period
+     * @param int   $slowD_MAType
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public static function slowstochrsi(array $real, int $rsi_period = 14, int $fastK_Period = 5, int $slowK_Period = 3, int $slowK_MAType = MovingAverageType::SMA, int $slowD_Period = 3, int $slowD_MAType = MovingAverageType::SMA): array
+    {
+        $real   = \array_values($real);
+        $endIdx = count($real) - 1;
+        $rsi    = [];
+        self::checkForError(self::getMomentumIndicators()::rsi(0, $endIdx, $real, $rsi_period, self::$outBegIdx, self::$outNBElement, $rsi));
+        $rsi      = array_values($rsi);
+        $endIdx   = self::verifyArrayCounts([&$rsi]);
+        $outSlowK = [];
+        $outSlowD = [];
+        self::checkForError(self::getMomentumIndicators()::stoch(0, $endIdx, $rsi, $rsi, $rsi, $fastK_Period, $slowK_Period, $slowK_MAType, $slowD_Period, $slowD_MAType, self::$outBegIdx, self::$outNBElement, $outSlowK, $outSlowD));
+
+        return [
+            'SlowK' => self::adjustIndexes($outSlowK, self::$outBegIdx),
+            'SlowD' => self::adjustIndexes($outSlowD, self::$outBegIdx),
+        ];
+    }
 
 }
--- tests/LupeTraderTest.php
+++ tests/LupeTraderTest.php
@@ -2,8 +2,27 @@
 
 namespace LupeCode\phpTraderNativeTest;
 
+use LupeCode\phpTraderNative\TALib\Enum\MovingAverageType;
+use LupeCode\phpTraderNative\LupeTrader;
 use LupeCode\phpTraderNativeTest\TraderTest;
 
 class LupeTraderTest extends TraderTest
 {
+
+    /**
+     * @throws \Exception
+     */
+    public function testSlowStochRsi()
+    {
+        $close = [4265.36, 4283.8, 3774.99, 3936.69, 3731.32, 3775, 4225.03, 4248, 3976, 4142.01, 4103.19, 3833.47, 3901.84, 3694.39, 3433.26, 3380.01, 3401, 3531.18, 3410.15, 3349.36, 3430.24, 3265, 3195.71, 3183, 3195];
+
+        $rsi_period        = 3;
+        $optInFastK_Period = 3;
+        $optInSlowK_Period = 14;
+        $optInSlowD_Period = 3;
+        $optInSlowK_MAType = MovingAverageType::SMA;
+        $optInSlowD_MAType = MovingAverageType::SMA;
+        $Output            = LupeTrader::slowstochrsi($close, $rsi_period, $optInFastK_Period, $optInSlowK_Period, $optInSlowK_MAType, $optInSlowD_Period, $optInSlowD_MAType);
+        var_export($Output);
+    }
 }
