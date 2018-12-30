8000be347af71d4fd4585811482c609c0a26496f
Joshua <joshua@lupecode.com>
2018-12-30 03:04:05 -0800

    Updated the documentation and added the friendly version for slow stoch rsi

--- source/LupeTrader.php
+++ source/LupeTrader.php
@@ -9,15 +9,17 @@ class LupeTrader extends Trader
 {
 
     /**
-     * @param array $real
-     * @param int   $rsi_period
-     * @param int   $fastK_Period
-     * @param int   $slowK_Period
-     * @param int   $slowK_MAType
-     * @param int   $slowD_Period
-     * @param int   $slowD_MAType
+     * Slow Stochastic Relative Strength Index
      *
-     * @return array
+     * @param array $real Array of real values.
+     * @param int   $rsi_period [OPTIONAL] [DEFAULT 14, SUGGESTED 4-200] Number of period. Valid range from 2 to 100000.
+     * @param int $fastK_Period [OPTIONAL] [DEFAULT 5, SUGGESTED 1-200] Time period for building the Fast-K line. Valid range from 1 to 100000.
+     * @param int $slowK_Period [OPTIONAL] [DEFAULT 3, SUGGESTED 1-200] Smoothing for making the Slow-K line. Valid range from 1 to 100000, usually set to 3.
+     * @param int $slowK_MAType [OPTIONAL] [DEFAULT TRADER_MA_TYPE_SMA] Type of Moving Average for Slow-K. MovingAverageType::* series of constants should be used.
+     * @param int $slowD_Period [OPTIONAL] [DEFAULT 3, SUGGESTED 1-200] Smoothing for making the Slow-D line. Valid range from 1 to 100000.
+     * @param int $slowD_MAType [OPTIONAL] [DEFAULT TRADER_MA_TYPE_SMA] Type of Moving Average for Slow-D. MovingAverageType::* series of constants should be used.
+     *
+     * @return array Returns an array with calculated data. [SlowK => [...], SlowD => [...]]
      * @throws \Exception
      */
     public static function slowstochrsi(array $real, int $rsi_period = 14, int $fastK_Period = 5, int $slowK_Period = 3, int $slowK_MAType = MovingAverageType::SMA, int $slowD_Period = 3, int $slowD_MAType = MovingAverageType::SMA): array
--- tests/LupeTraderFriendlyTest.php
+++ tests/LupeTraderFriendlyTest.php
@@ -2,9 +2,27 @@
 
 namespace LupeCode\phpTraderNativeTest;
 
+use LupeCode\phpTraderNative\LupeTraderFriendly;
+use LupeCode\phpTraderNative\TALib\Enum\MovingAverageType;
 use LupeCode\phpTraderNativeTest\TraderFriendlyTest;
 
 class LupeTraderFriendlyTest extends TraderFriendlyTest
 {
-
+    /**
+     * @throws \Exception
+     */
+    public function testSlowStochasticRelativeStrengthIndex()
+    {
+        $rsi_period        = 14;
+        $optInFastK_Period = 3;
+        $optInSlowK_Period = 10;
+        $optInSlowD_Period = 5;
+        $optInSlowK_MAType = MovingAverageType::SMA;
+        $optInSlowD_MAType = MovingAverageType::SMA;
+        $Output            = LupeTraderFriendly::slowStochasticRelativeStrengthIndex($this->Close, $rsi_period, $optInFastK_Period, $optInSlowK_Period, $optInSlowK_MAType, $optInSlowD_Period, $optInSlowD_MAType);
+        $traderRsi         = \trader_rsi($this->Close, $rsi_period);
+        list($traderSlowK, $traderSlowD) = \trader_stoch($traderRsi, $traderRsi, $traderRsi, $optInFastK_Period, $optInSlowK_Period, $optInSlowK_MAType, $optInSlowD_Period, $optInSlowD_MAType);
+        $this->assertEquals($traderSlowK, $this->adjustForPECL($Output['SlowK']), '', 0.1);
+        $this->assertEquals($traderSlowD, $this->adjustForPECL($Output['SlowD']), '', 0.1);
+    }
 }
